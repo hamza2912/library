7157180e0879889ba79e696306862c3c6c8675fb
Hamza Ali <hamzaali2912@gmail.com>
2021-06-23 12:02:03 -0700

    Home screen edited

--- my-app/src/components/sidebar.js
+++ my-app/src/components/sidebar.js
@@ -1,16 +1,16 @@
 function Sidebar() {
     return (
   
-      <div className='w-20 h-screen flex flex-col items-center px-2 py-8 bg-sidebar fixed'>
-            <div className='catalog'>
+      <div className='w-20 h-screen flex flex-col items-center px-2 py-8 bg-blue fixed'>
+            <div className='home'>
                 <a className='link-home' href=""><i class="fas fa-book-open text-white text-xl"></i></a>
             </div>
             <div className='flex flex-col items-center mt-24 text-white text-xl'>
-                <a className='link-about' href="#about"><i className="fas fa-users mb-10"></i></a>   
-                <a className='link-events' href=""><i class="far fa-calendar-alt mb-10"></i></a>
-                <a className='link-services' href=""><i class="fas fa-hand-holding mb-10"></i></a>
-                <a className='link-view' href=""><i class="fas fa-street-view mb-10"></i></a>       
-        </div>
+                <a className='link-about mb-10' href="#about"><i className="fas fa-users "></i></a>   
+                <a className='link-events mb-10' href=""><i class="far fa-calendar-alt "></i></a>
+                <a className='link-services mb-10' href=""><i class="fas fa-hand-holding "></i></a>
+                <a className='link-view mb-10' href=""><i class="fas fa-street-view "></i></a>       
+            </div>
       </div>
 
     );
--- my-app/src/pages/home.js
+++ my-app/src/pages/home.js
@@ -5,15 +5,109 @@ function Home() {
       
       <div>
         <Sidebar />
+
+        <div className='pl-40 pr-32'>
+
         {/* Main Screen */}
-        <div className='flex flex-row w-full h-screen pt-8'>
-          <div className='w-2/3 pl-20'>
-            <p>Hello</p>
+          <div className='flex flex-row w-full pt-8'>
+            <div className='w-2/3'>
+              <div className='w-full bg-search rounded-lg px-5 py-2 flex flex-row justify-between text-xs text-gray-700'>
+                  <p>Search</p>
+                  <p><i class="fas fa-search"></i></p> 
+              </div>
+            </div>
+            <div className='w-1/3 flex flex-row justify-end'>
+              <div className='w-1/3 flex flex-row justify-between items-center text-blue'>
+                  <a href=""><i class="fas fa-user text-white bg-orange rounded-full p-2"></i></a>
+                  <p>Aziz</p>
+                  <a href=""><i class="fas fa-angle-down"></i></a>
+              </div>
+
+            </div>
           </div>
-          <div className='w-1/3 flex justify-start items-center'>
-            <img className='w-full h-auto' src="big-logo.png" alt="JT Logo Big" />
+          <div className='flex flex-row w-full h-screen pt-6'>
+            <div className='w-2/3 flex flex-col'>
+              <div className='w-full h-video rounded-lg bg-black relative'>
+                <div className='w-1/3 bg-white rounded-lg absolute bottom-0 mb-5 ml-5 border-2 border-pink-400'>
+                  <div className='relative px-4 py-2'>
+                    <p className='text-xs text-gray-700'>Subscribe now</p>
+                    <div className='w-1/5 h-full bg-pink-400 rounded-r-lg absolute top-0 right-0 flex justify-center items-center'>
+                        <a className='' href=""><i class="fas fa-envelope text-white text-sm"></i></a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div className='w-full grid grid-cols-2 gap-4 mt-5'>
+              <div>
+                <p className='mb-2'>Upcoming Event</p>
+                <div className='w-full h-64 rounded-lg bg-black relative'>
+                </div>
+              </div>
+              <div>
+                <p className='mb-2'>Most Recent</p>
+                <div className='w-full h-64 rounded-lg bg-black relative'>
+                </div>
+              </div>
+            </div>
+
+            </div>
+
+            <div className='w-1/3 flex flex-col pl-10'>
+              <div class='w-full mx-auto'>
+                <img className='object-contain w-3/5 h-auto mx-auto' src="jt-logo.png" alt="JT Logo" />
+              </div>
+
+              <div>
+                <p className='my-2'>Best Seller Books</p>
+                <div className='w-full grid grid-cols-4 gap-4'>
+                  <div className='flex flex-col'>
+                    <div className='h-32 bg-black mb-2'>
+                    </div>
+                    <p>Book Name</p>
+                  </div>
+                  <div className='flex flex-col'>
+                    <div className='h-32 bg-black mb-2'>
+                    </div>
+                    <p>Book Name</p>
+                  </div>
+                  <div className='flex flex-col'>
+                    <div className='h-32 bg-black mb-2'>
+                    </div>
+                    <p>Book Name</p>
+                  </div>
+                  <div className='flex flex-col'>
+                    <div className='h-32 bg-black mb-2'>
+                    </div>
+                    <p>Book Name</p>
+                  </div>
+                </div>
+              </div>
+
+
+            </div>
+            
           </div>
+
+            
+
+
+
+          {/* <div className='flex flex-row w-full h-auto pt-5'>
+            
+          </div> */}
+
+
+
         </div>
+        
+        
+
+
+
+
+
+
       </div> 
    
     );
